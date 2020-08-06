1a64b29742d98cad9050caa33961b0775d19de9c
Khushal Uttam <khushaluttam@gmail.com>
2020-08-06 22:28:03 +0530

    otp to mail verfication

--- app/src/main/java/com/example/jagratiapp/signup_page.java
+++ app/src/main/java/com/example/jagratiapp/signup_page.java
@@ -6,21 +6,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class signup_page extends AppCompatActivity {
+    //ToDO: Commented wale abhi use nahi kar rahe
     private ImageView img;
     private EditText email;
     private EditText password;
     private EditText cpassword;
-    private EditText Eotp;
-    private EditText phoneno;
-    private Button getotp;
+//    private EditText Eotp;
+//    private EditText phoneno;
+//    private Button getotp;
     private Button signup;
-    private TextView errortext;
-    private TextView countertext;
+//    private TextView errortext;
+//    private TextView countertext;
+
+    private ProgressBar progressBar;
+
+    //Firebase authorisation
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +40,8 @@ public class signup_page extends AppCompatActivity {
         setContentView(R.layout.activity_signup_page);
         img = findViewById(R.id.signup_background);
         img.setImageAlpha(50);
-        countertext = findViewById(R.id.countertext);
-        getotp = findViewById(R.id.sign_get_otp);
+//        countertext = findViewById(R.id.countertext);
+//        getotp = findViewById(R.id.sign_get_otp);
 
         View decorView = getWindow().getDecorView();
 
@@ -38,26 +50,43 @@ public class signup_page extends AppCompatActivity {
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
         decorView.setSystemUiVisibility(uiOptions);
-        Eotp = findViewById(R.id.signup_otp);
-        Eotp.setVisibility(View.INVISIBLE);
+//        Eotp = findViewById(R.id.signup_otp);
+//        Eotp.setVisibility(View.INVISIBLE);
 
-        getotp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new CountDownTimer(30000, 1000) {
-
-                    public void onTick(long millisUntilFinished) {
-                        countertext.setText("Create OTP again in " + millisUntilFinished / 1000 + " seconds");
-                        getotp.setEnabled(false);
-                        Eotp.setVisibility(View.VISIBLE);
-                    }
+        //starting from here
+        email = findViewById(R.id.sign_up_email);
+        password = findViewById(R.id.signup_pass);
+        cpassword = findViewById(R.id.signup_pass_again);
 
 
-                    public void onFinish() {
-                        countertext.setText("Get OTP again");
-                        getotp.setEnabled(true);
-                    }
-                }.start();
+
+
+
+
+
+
+
+
+
+
+
+//        getotp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                new CountDownTimer(30000, 1000) {
+//
+//                    public void onTick(long millisUntilFinished) {
+//                        countertext.setText("Create OTP again in " + millisUntilFinished / 1000 + " seconds");
+//                        getotp.setEnabled(false);
+//                        Eotp.setVisibility(View.VISIBLE);
+//                    }
+//
+//
+//                    public void onFinish() {
+//                        countertext.setText("Get OTP again");
+//                        getotp.setEnabled(true);
+//                    }
+//                }.start();
 
             }
         });
