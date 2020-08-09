c280ff67062970d42c57e7b5d6ef6709332b9a0a
harsh-hash <2018099@iiitdmj.ac.in>
2020-08-10 01:37:14 +0530

    Login Added

--- app/src/main/java/com/example/jagratiapp/login_page.java
+++ app/src/main/java/com/example/jagratiapp/login_page.java
@@ -5,42 +5,87 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 //hi this an artificial comment!
 
-public class login_page extends AppCompatActivity {
+public class login_page extends AppCompatActivity{
     private ImageView img;
-    private TextView forget_pass;
-    private Button loginBut;
+    private TextView forgetPasswordLogin;
+    private Button signUpButtonLogin;
+    private Button loginButtonLogin;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+
+    private FirebaseAuth firebaseAuth;
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-        img = findViewById(R.id.login_background);
+        img = findViewById(R.id.background);
         img.setImageAlpha(50);
+        findViews();
 
-        loginBut = findViewById(R.id.login_but);
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        loginBut.setOnClickListener(new View.OnClickListener() {
+        loginButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkLogin();
+            }
+        });
+
+        signUpButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(login_page.this,signup_page.class));
             }
         });
 
-        forget_pass = findViewById(R.id.forget_pass);
-        forget_pass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(login_page.this, "Working",Toast.LENGTH_SHORT).show();
-            }
-        });
+    }
+
+    public void checkLogin(){
+
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(login_page.this,HomePage.class));
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(login_page.this,e.getMessage().toString().trim(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+
+    public void findViews(){
+        loginButtonLogin = findViewById(R.id.login_button_login);
+        signUpButtonLogin = findViewById(R.id.signUp_button_login);
+        forgetPasswordLogin = findViewById(R.id.forget_password_button_login);
+        emailEditText = findViewById(R.id.login_email);
+        passwordEditText = findViewById(R.id.login_password);
+
     }
 }
