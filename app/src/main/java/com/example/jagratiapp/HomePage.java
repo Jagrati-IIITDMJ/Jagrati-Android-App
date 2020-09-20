2c6b78f98e7c3ad97cf0105d3e63cb057172fc7b
AkshaySh007 <47864221+akshaysh007@users.noreply.github.com>
2020-09-20 19:09:03 +0530

    ui changes 3

--- app/src/main/java/com/example/jagratiapp/HomePage.java
+++ app/src/main/java/com/example/jagratiapp/HomePage.java
@@ -1,23 +1,39 @@
 package com.example.jagratiapp;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private ImageView profile_button;
-    private Button signout_button;
+
     private Button classes;
     private Button quizzes;
     private FirebaseAuth.AuthStateListener authStateListener;
+    private DrawerLayout drawer;
+    private AlertDialog dialog;
+    private AlertDialog.Builder builder;
+    private Button yes;
+    private Button no;
 
     private FirebaseAuth firebaseAuth;
 
@@ -26,15 +42,21 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         findViews();
+        MaterialToolbar toolbar = findViewById(R.id.attendance_toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.draweropen,R.string.drawerclosed);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.jag1));
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
         firebaseAuth = FirebaseAuth.getInstance();
-        signout_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signOut();
 
-            }
-        });
 
         classes.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,12 +65,7 @@ public class HomePage extends AppCompatActivity {
             }
         });
 
-        profile_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HomePage.this,VolunteerProfile.class));
-            }
-        });
+
 
         quizzes.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,19 +78,74 @@ public class HomePage extends AppCompatActivity {
 
     }
 
+
     private void signOut(){
             firebaseAuth.signOut();
             startActivity(new Intent(HomePage.this,StartPage.class));
-            finish();
+            finishAffinity();
 
     }
 
 
     private void findViews() {
-        signout_button = findViewById(R.id.signoutButton);
+
         classes = findViewById(R.id.classes);
-        profile_button = findViewById(R.id.profile);
         quizzes = findViewById(R.id.quiz);
+
+
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.profile_nav:
+                startActivity(new Intent(HomePage.this,VolunteerProfile.class));
+                break;
+
+            case R.id.sign_out_nav:
+                createSignOutPop();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void createSignOutPop() {
+        builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.singout_pop,null);
+
+        yes = view.findViewById(R.id.confirml_signout);
+        no= view.findViewById(R.id.cancel_signout);
+
+        builder.setView(view);
+        dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+                dialog.dismiss();
+
+            }
+        });
+
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START))
+            drawer.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
 }
