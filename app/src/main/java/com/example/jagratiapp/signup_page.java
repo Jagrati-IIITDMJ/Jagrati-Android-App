680538547f80e2a194433708f839cbe826078883
harsh-hash <2018099@iiitdmj.ac.in>
2020-10-08 13:13:41 +0530

    edit and save student id

--- app/src/main/java/com/example/jagratiapp/signup_page.java
+++ app/src/main/java/com/example/jagratiapp/signup_page.java
@@ -2,36 +2,26 @@ package com.example.jagratiapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.ThrowOnExtraProperties;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 public class signup_page extends AppCompatActivity {
 
@@ -101,7 +91,11 @@ public class signup_page extends AppCompatActivity {
                     String name = name_edit_signup.getText().toString().trim();
                     String cpassword = cpassword_edit_signup.getText().toString().trim();
 
-                    createAccount(email,password,name);
+                    if (password != cpassword){
+                        cpassword_edit_signup.setError("password same dalo beta");
+                    }
+                    else
+                        createAccount(email,password,name);
 
 
                 }else{
